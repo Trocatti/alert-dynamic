2ba1b935148417ac7bea7969b422b74fdf72f6c6
Guilherme Trocatti <guilhermetrocatti@hotmail.com>
2021-01-14 18:25:06 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <Alert :binds="notifications" />
+  </div>
+</template>
+
+<script>
+import Alert from "./components/Alert";
+
+export default {
+  name: "app",
+  components: {
+    Alert,
+  },
+  data() {
+    return {
+      notifications: [
+        {
+          color: "warning",
+          title: "alert.internetDisconnect.title",
+          subtitle: "alert.internetDisconnect.subtitle",
+          icon: {
+            component: "InternetDisconnectIcon",
+          },
+          dismissible: false,
+          fade: true,
+          show: true,
+          link: {
+            href: "http://google.com",
+            title: "alert.internetDisconnect.resolve",
+          },
+        },
+        {
+          color: "success",
+          title: "alert.internetSuccess.title",
+          icon: {
+            component: "InternetSuccessIcon",
+          },
+          dismissible: true,
+          fade: true,
+          show: true,
+        },
+        {
+          color: "danger",
+          title: "alert.internetDisconnect.title",
+          subtitle: "alert.internetDisconnect.subtitle",
+          icon: {
+            component: "InternetSuccessIcon",
+          },
+          dismissible: false,
+          fade: true,
+          show: true,
+          tooltip: {
+            hover: "991074224, 99153214",
+            title: "alert.internetDisconnect.tooltip",
+          },
+        },
+        {
+          color: "danger",
+          title: "alert.internetDisconnect.title",
+          subtitle: "alert.internetDisconnect.subtitle",
+          icon: {
+            component: "InternetDisconnectIcon",
+            color: "white",
+          },
+          dismissible: false,
+          fade: true,
+          show: true,
+          modal: {
+            title: "alert.internetDisconnect.resolve",
+            component: "WhatsAppDisconnectModal",
+            params: {
+              numbers: [
+                {
+                  name: "Financeiro",
+                  number: "991074224",
+                },
+                {
+                  name: "Vendas",
+                  number: "991074224",
+                },
+                {
+                  name: "Suporte",
+                  number: "991074224",
+                },
+              ],
+            },
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/WhatsAppDisconnectModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-modal v-model="modalShow">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "whatsappDisconnectModal",
+  props: {
+    show: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    params: {
+      type: Object,
+    },
+  },
+  computed: {
+    modalShow() {
+      return this.show;
+    },
+  },
+};
+</script>
